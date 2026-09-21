57ab2197affe03f7744148832296d178eb16ccef
illhammf <illhammf@users.noreply.github.com>
2026-09-21 20:46:21 +0700

    update LuasSegitiga.java

--- src/pertemuan-01/quiz/LuasSegitiga.java
+++ src/pertemuan-01/quiz/LuasSegitiga.java
@@ -1,5 +1,25 @@
-package pertemuan-01.quiz;
+import java.util.Scanner; // Import class Scanner untuk input dari user
 
 public class LuasSegitiga {
-    
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Input alas
+        System.out.print("Masukkan alas segitiga: ");
+        double alas = input.nextDouble();
+
+        // Input tinggi
+        System.out.print("Masukkan tinggi segitiga: ");
+        double tinggi = input.nextDouble();
+
+        // Menghitung luas
+        double luas = 0.5 * alas * tinggi;
+
+        // Menampilkan hasil
+        System.out.println("Luas segitiga = " + luas);
+
+        input.close();
+    }
 }
