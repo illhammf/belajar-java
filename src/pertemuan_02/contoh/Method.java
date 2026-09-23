0dc810fd7de6d224dc598c1ebea1c6ec52c4329c
illhammf <illhammf@users.noreply.github.com>
2026-09-23 21:37:27 +0700

    add MathMethod

--- src/pertemuan_02/contoh/Method.java
+++ src/pertemuan_02/contoh/Method.java
@@ -1,5 +1,30 @@
 package pertemuan_02.contoh;
 
 public class Method {
-    
+
+    // Method tanpa parameter
+    static void salam() {
+        System.out.println("Halo, selamat belajar Java!");
+    }
+
+    // Method dengan parameter
+    static void tampilkanNama(String nama) {
+        System.out.println("Nama saya " + nama);
+    }
+
+    // Method dengan return value
+    static int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        salam();
+
+        tampilkanNama("Ilham");
+
+        int hasil = tambah(10, 5);
+
+        System.out.println("Hasil penjumlahan : " + hasil);
+    }
 }
