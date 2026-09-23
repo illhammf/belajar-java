5c106437312c089579c11e776dc0b35c45da55ef
illhammf <illhammf@users.noreply.github.com>
2026-09-23 21:47:21 +0700

    update quiz pert 2

--- src/pertemuan_02/quiz/KalkulatorMatematika.java
+++ src/pertemuan_02/quiz/KalkulatorMatematika.java
@@ -1,5 +1,26 @@
 package pertemuan_02.quiz;
 
-public t  {
-    
+public class KalkulatorMatematika {
+
+    // Method untuk menghitung sisi miring segitiga
+    static double hitungSisiMiring(double a, double b) {
+
+        // Rumus: √(a² + b²)
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+
+    public static void main(String[] args) {
+
+        // Tipe data dan variabel
+        double sisiA = 3;
+        double sisiB = 4;
+
+        // Memanggil method
+        double sisiMiring = hitungSisiMiring(sisiA, sisiB);
+
+        // Menampilkan hasil
+        System.out.println("Sisi A       : " + sisiA);
+        System.out.println("Sisi B       : " + sisiB);
+        System.out.println("Sisi Miring  : " + sisiMiring);
+    }
 }
