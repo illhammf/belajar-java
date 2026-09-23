5e3463d7c3dc11b560758139ba5148ee674bb9cc
illhammf <illhammf@users.noreply.github.com>
2026-09-23 11:57:14 +0700

    update Variabel

--- src/pertemuan_02/contoh/Variabel.java
+++ src/pertemuan_02/contoh/Variabel.java
@@ -1,5 +1,19 @@
 package pertemuan_02.contoh;
 
 public class Variabel {
-    
+
+    public static void main(String[] args) {
+
+        // Deklarasi dan pemberian nilai
+        String nama = "Ilham";
+        int umur = 20;
+
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+
+        // Mengubah nilai variabel
+        umur = 21;
+
+        System.out.println("Umur setelah diubah : " + umur);
+    }
 }
