8b7244c032f0612f5c71f393ef03e97ba557dc2d
illhammf <illhammf@users.noreply.github.com>
2026-09-23 11:52:44 +0700

    update tipe data

--- src/pertemuan_02/contoh/TipeData.java
+++ src/pertemuan_02/contoh/TipeData.java
@@ -2,4 +2,23 @@ package pertemuan_02.contoh;
 
 public class TipeData {
     
+    public static void main(String[] args) {
+
+        // Tipe data integer
+        int umur = 20;
+
+        // Tipe data pecahan
+        double tinggi = 170.5;
+
+        // Tipe data karakter
+        char jenisKelamin = 'L';
+
+        // Tipe data boolean
+        boolean mahasiswa = true;
+
+        System.out.println("Umur          : " + umur);
+        System.out.println("Tinggi        : " + tinggi);
+        System.out.println("Jenis Kelamin : " + jenisKelamin);
+        System.out.println("Mahasiswa     : " + mahasiswa);
+    }
 }
